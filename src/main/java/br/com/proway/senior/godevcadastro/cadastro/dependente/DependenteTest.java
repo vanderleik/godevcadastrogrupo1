05e4dd184b97414f449893016d626ca624859880
Samuel Alves <samuel.levi.alves@outlook.com>
2021-04-16 13:28:01 -0300

    Teste da classe

---
+++ src/main/java/br/com/proway/senior/godevcadastro/cadastro/dependente/DependenteTest.java
@@ -0,0 +1,14 @@
+package br.com.proway.senior.godevcadastro.cadastro.dependente;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DependenteTest {
+
+	@Test
+	public void testDependenteStringStringDateStringStringCharStringStringStringBooleanLongLongStringBoolean() {
+		fail("Not yet implemented");
+	}
+
+}
