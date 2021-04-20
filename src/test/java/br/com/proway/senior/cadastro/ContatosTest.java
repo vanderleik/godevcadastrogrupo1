dbb8657660728944d0d02254c1c2d05db45ae8b2
SENIOR <SENIOR@proway.treina>
2021-04-20 11:13:30 -0300

    Validacoes, metodos e testes contatos

--- src/test/java/br/com/proway/senior/cadastro/ContatosTest.java
+++ src/test/java/br/com/proway/senior/cadastro/ContatosTest.java
@@ -1,12 +1,12 @@
 package br.com.proway.senior.cadastro;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
 import org.junit.Test;
 
-import br.com.proway.senior.godevcadastro.cadastro.colaborador.Colaborador;
 import br.com.proway.senior.godevcadastro.cadastro.contatos.Contatos;
 
 public class ContatosTest {
@@ -31,9 +31,8 @@ public class ContatosTest {
 
 	@Test
 	public void testSetTelefonePrincipal() {
-		Contatos contatos = new Contatos("47987654321", "47123456789", "123@teste.com.br");
-		contatos.setTelefonePrincipal("12345678910");
-		assertEquals(contatos.getTelefonePrincipal(), "12345678910");
+		Contatos contatos = new Contatos("(47) 99999-9999", "47123456789", "123@teste.com.br");
+		assertEquals("47999999999" , contatos.getTelefonePrincipal());
 	}
 
 	@Test
@@ -46,8 +45,7 @@ public class ContatosTest {
 	@Test
 	public void testSetEmail() {
 		Contatos contatos = new Contatos("47987654321", "47123456789", "123@teste.com.br");
-		contatos.setEmail("abcd@teste.com");
-		assertEquals(contatos.getEmail(), "abcd@teste.com");
+		assertEquals("123@teste.com.br", contatos.getEmail());
 	}
 
 	@Test
@@ -63,5 +61,53 @@ public class ContatosTest {
 		Contatos.cadastrarContato(contatos, contato);
 		assertTrue(contatos.indexOf(contato)>=0);
 	}
+	
+	@Test
+	public void testListarUmContato() {
+		Contatos contato1 = new Contatos("48953627858", "48573459863", "456@teste.com.br");
+		Contatos.cadastrarContato(contatos, contato1);
+		Contatos contato2 = new Contatos("47987654321", "47123456789", "123@teste.com.br");
+		Contatos.cadastrarContato(contatos, contato2);
+		assertEquals("48953627858", Contatos.listarContato(contatos, 0).getTelefonePrincipal());
+		assertEquals("48573459863", Contatos.listarContato(contatos, 0).getTelefoneSecundario());
+		assertEquals("456@teste.com.br", Contatos.listarContato(contatos, 0).getEmail());
+	}
 
+	@Test
+	public void testAtualizarUmContato() {
+		Contatos contato1 = new Contatos("48953627858", "48573459863", "456@teste.com.br");
+		Contatos.cadastrarContato(contatos, contato1);
+		Contatos contato2 = new Contatos("47987654321", "47123456789", "123@teste.com.br");
+		Contatos.atualizarUmContato(contatos, contato2, 0);
+		assertEquals("47987654321", contatos.get(0).getTelefonePrincipal());
+		assertEquals("123@teste.com.br", contatos.get(0).getEmail());
+	}
+	
+	@Test
+	public void testRemoverUmContatoPorId() {
+		Contatos contato1 = new Contatos("48953627858", "48573459863", "456@teste.com.br");
+		Contatos.cadastrarContato(contatos, contato1);
+		Contatos contato2 = new Contatos("47987654321", "47123456789", "123@teste.com.br");
+		Contatos.cadastrarContato(contatos, contato2);
+		Contatos.removerUmContato(contatos, 0);
+		assertEquals("47987654321", contatos.get(0).getTelefonePrincipal());
+		assertEquals("123@teste.com.br", contatos.get(0).getEmail());
+	}
+	
+	@Test
+	public void testRemoverUmContatoPassandoUmContato() {
+		Contatos contato1 = new Contatos("48953627858", "48573459863", "456@teste.com.br");
+		Contatos.cadastrarContato(contatos, contato1);
+		Contatos contato2 = new Contatos("47987654321", "47123456789", "123@teste.com.br");
+		Contatos.cadastrarContato(contatos, contato2);
+		Contatos.removerUmContatoPassandoUmContato(contatos, contato1);
+		assertEquals("47987654321", contatos.get(0).getTelefonePrincipal());
+		assertEquals("123@teste.com.br", contatos.get(0).getEmail());
+	}
+	
+	
+	
+	
+	
+	
 }
