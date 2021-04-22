0570a8aca40dde13e4cc5e353e2c65fb3e5c4cbd
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-04-22 09:45:59 -0300

    Atualizando enum UF

--- src/main/java/br/com/proway/senior/godevcadastro/cadastro/enums/UnidadesFederativas.java
+++ src/main/java/br/com/proway/senior/godevcadastro/cadastro/enums/UnidadesFederativas.java
@@ -8,7 +8,6 @@ public enum UnidadesFederativas {
 	 * 
 	 * @author Lorran Pereira dos Santos, Samuel Levi, Sarah Neuburger Brito, Thiago Luiz Barbieri e Vitor Nathan Gonçalves.
 	 */
-	
-	SC, RS, RN, SP
+	AC, AL, AP, AM, BA, CE, DF, ES, GO, MA, MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO
 
 }
