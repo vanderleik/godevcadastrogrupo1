b92ba5d01295b2d7355eb98f8e50268e9acf5c3c
KenjiNishi <wk.nishizawa@gmail.com>
2021-05-04 17:30:25 -0300

    comments on DataParser

--- src/main/java/controller/DataParser.java
+++ src/main/java/controller/DataParser.java
@@ -4,10 +4,22 @@ import java.time.LocalDate;
 import java.sql.Date;
 
 public class DataParser {
+	/**
+	 * Passa de LocalDate para java.sql.Date esperado pelo banco de dados
+	 * @param entrada em formato LocalDate 
+	 * @author Willian Kenji Nishizawa <wk.nishizawa@gmail.com>
+	 * @return entrada em Date
+	 */
 	public static Date localDateToSQLDate(LocalDate entrada) {
 		return Date.valueOf(entrada);
 	}
 	
+	/**
+	 * Passa de sql.Date vindo do Banco de Dados para java.time.LocalDate
+	 * @param entrada em formato Date 
+	 * @author Willian Kenji Nishizawa <wk.nishizawa@gmail.com>
+	 * @return entrada em LocalDate
+	 */
 	public static LocalDate sqlDateToLocalDate(Date entrada) {
 		return entrada.toLocalDate();
 	}
@@ -17,8 +29,8 @@ public class DataParser {
 	 * utilizavel em Queries. 
 	 * 
 	 * @author Willian Kenji Nishizawa <wk.nishizawa@gmail.com>
-	 * @param entrada
-	 * @return String
+	 * @param entrada data em LocalDate
+	 * @return String para ser usada em Query
 	 */
 	public static String localDateToSQLQuery(LocalDate entrada) {
 		return "'"+entrada.toString()+"'";
